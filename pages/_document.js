7ee045579de28b8a80cb431b050ac11bf4b2ad8d
Dayo Jiboye <dayojiboye2@gmail.com>
2021-02-23 10:47:31 +0100

    added description for SEO and a custom _document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -7,7 +7,15 @@ class MyDocument extends Document {
   }
 
   render() {
-    return <Html lang="en"></Html>;
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
